5aa16326fe23a84cf8b710ae0688c3c9292510f1
execu <execu@10.0.0.166>
2020-05-26 20:08:15 -0400

    Update

--- src/org/sqlite/core/NativeDB.java
+++ src/org/sqlite/core/NativeDB.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 David Crawshaw <david@zentus.com>
+ * Copyright (c) 2007 Executive Order Corporation
  * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
